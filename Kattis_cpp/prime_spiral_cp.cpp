6ae8b2b0485ea359e3c713d33001b577f19cbfc3
d0rshBooken <andvr916@student.liu.se>
2019-09-03 21:12:30 +0200

    C++ version done

--- Kattis_cpp/prime_spiral_cp.cpp
+++ Kattis_cpp/prime_spiral_cp.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <queue>
+
 
 using namespace std;
 
@@ -7,8 +10,6 @@ using namespace std;
 #define MID_X 50
 #define MID_Y 50
 
-int dx[4] = {-1,0,0,1};
-int dy[4] = {0,-1,1,0};
 
 
 // Creed GeekForGeeks 
@@ -26,70 +27,39 @@ bool isPrime(int n)
 } 
 
 
-struct Coord {
-    Coord()                 {   this->x = 10001; this->y = 10001;     }
-    Coord(int _x, int _y)   {   this->x = _x; this->y = _y;    }
-     int x;
-     int y;
-
-    int getInt()            {   return get(this->x, this->y);   }
-    /*Coord onePlus()          
-    {
-        if (++(this->x) == SIZE_X)
-        {
-            this->x = 0;
-            this->y += 1;
-        }
-        else 
-        {
-            this->x += 1;
-        }
-        return *this;
-    }*/
-};
-
-Coord get(int num) 
-{
-    return Coord(num % SIZE_X, num / SIZE_X);
-}
-
-// Convert coordinates to number
-int get(int x, int y)
-{
-    return ((y + MID_Y) * SIZE_Y) + (x + MID_X);
-}
-
 
 int main() {
-    int currx = 0;
-    int curry = 0;
+    int x = 0;
+    int y = 0;
+    int dx[4] = {-1,0,0,1};
+    int dy[4] = {0,-1,1,0};
 
-    map<int, int> to_num;
-    Coord to_coord[10001]; 
+    map<pair<int,int>, int> to_num;
+    pair<int,int> to_coord[10001]; 
 
-    to_num[get(currx,curry)] = 1;
-    to_coord[1] = Coord{currx,curry};
+    to_num[{x,y}] = 1;
+    to_coord[1] = {x,y};
 
     int curr = 2;
     int steps = 1;
     while(curr <= 10000) {
         for(int i = 0; i < 4 && curr <= 10000; i++) {
             for(int j = 0; j < steps && curr <= 10000; j++) {
-                if(i == 0) {
-                    curry++;
-                }
-                if(i == 1) {
-                    currx--;
-                }
-                if(i == 2) { curry--;
-                }
-                if(i == 3) {
-                    currx++;
+                switch(i) {
+                    case 0: y++; break;
+                    case 1: x--; break;
+                    case 2: y--; break;
+                    case 3: x++; break;
                 }
 
-                if(!isPrime(curr)) {
-                    to_num[get(currx,curry)] = curr;
-                    to_coord[curr] = Coord{currx,curry};
+                //  Skip if Prime number
+                if(isPrime(curr)) {
+                    ;
+                }
+                //  Add if Composite number
+                else {
+                    to_num[{x,y}] = curr;
+                    to_coord[curr] = {x,y};
                 }
 
                 curr++;
@@ -101,14 +71,13 @@ int main() {
     }
 
     
-    for(int i = -9; i <= 9; i++) {
+    for(int i = -9; i <= 9; i++) 
+    {
         for(int j = -9; j <= 9; j++) {
-           if (to_num.count(get(i,j)))
-           {
-                cout << to_num[get(i,j)] << "\t";
+           if (to_num.count({i,j}) ) {
+                cout << to_num[{i,j}] << "\t";
            }
-           else 
-           {
+           else {
                cout << " X " << "\t";
            }
         }
@@ -116,41 +85,47 @@ int main() {
     }
     
 
-    int num = 1;
-    int n1, n2;
-    while(cin >> n1 >> n2) {
-        cout << "Case " << num++ << ": ";
+    int _case = 1;
+    int src, dest;
+    while(cin >> src >> dest) {
+        cout << "Case " << _case++ << ": ";
 
-        map<int, int> vis;
-        queue<int> q;
-        q.push(to_coord[n1].getInt());
+        map<pair<int,int>, int> bfs_map;
+        queue<pair<int,int> > bfs_q;
+        bfs_q.push(to_coord[src]);
 
-        while(!q.empty()) {
-            int curr = q.front();
-            q.pop();
+        //
+        // Breadth First Search (BFS)
+        //
+        while(!bfs_q.empty()) {
+            auto curr = bfs_q.front();
+            bfs_q.pop();
 
             for(int i = 0; i < 4; i++) {
-                Coord next = get(curr);
-                next.x += dx[i];
-                next.y += dy[i];
+                auto next = curr;
+                
+                next.first += dx[i];
+                next.second += dy[i];
 
                 // Check if path is possible 
-                if(!to_num.count(get(next.x,next.y))) {
+                if(!to_num.count(next)) {
                     continue;
                 }
-                if(vis.count(next.getInt())) {
+                // Check if there already exists shorter path to coordinate
+                else if(bfs_map.count(next)) {
                     continue;
                 }
-                vis[next.getInt()] = vis[curr] + 1;
-                q.push(next.getInt());
+                bfs_map[next] = bfs_map[curr] + 1;
+                bfs_q.push(next);
             }
         }
 
-        if(vis[to_coord[n2].getInt()] == 0 && n1 != n2) {
+
+        // If dest is not inside map then no path available
+        if(!bfs_map.count(to_coord[dest])) {
             cout << "impossible" << endl;
-        }
-        else {
-            cout << vis[to_coord[n2].getInt()] << endl;
+        } else {
+            cout << bfs_map[to_coord[dest]] << endl;
         }
     }
 }
