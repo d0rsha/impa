846e6b9a9b4dfb6c2810ab8f9520443d1e7704e1
Anders Vrethem <anders.vrethem@hiq.se>
2019-09-03 16:42:35 +0200

    Fails Compilation

--- Kattis_cpp/prime_spiral_cp.cpp
+++ Kattis_cpp/prime_spiral_cp.cpp
@@ -1,31 +1,74 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int inf = 1 << 30;
+
+#define SIZE_X 100
+#define SIZE_Y 100
+#define MID_X 50
+#define MID_Y 50
 
 int dx[4] = {-1,0,0,1};
 int dy[4] = {0,-1,1,0};
 
-bool prime(int n) {
-    if(n <= 2) return true;
-    if(n % 2 == 0) return false;
-    for(int i = 3; i <= sqrt(n); i += 2) {
-        if(n % i == 0) {
-            return false;
+
+// Creed GeekForGeeks 
+bool isPrime(int n) 
+{ 
+	// Corner case 
+	if (n <= 1) 
+		return false; 
+	// Check from 2 to n-1 
+	for (int i = 2; i < n; i++) 
+		if (n % i == 0) 
+			return false; 
+
+	return true; 
+} 
+
+
+struct Coord {
+    Coord()                 {   this->x = 10001; this->y = 10001;     }
+    Coord(int _x, int _y)   {   this->x = _x; this->y = _y;    }
+     int x;
+     int y;
+
+    int getInt()            {   return get(this->x, this->y);   }
+    /*Coord onePlus()          
+    {
+        if (++(this->x) == SIZE_X)
+        {
+            this->x = 0;
+            this->y += 1;
         }
-    }
-    return true;
+        else 
+        {
+            this->x += 1;
+        }
+        return *this;
+    }*/
+};
+
+Coord get(int num) 
+{
+    return Coord(num % SIZE_X, num / SIZE_X);
 }
+
+// Convert coordinates to number
+int get(int x, int y)
+{
+    return ((y + MID_Y) * SIZE_Y) + (x + MID_X);
+}
+
 
 int main() {
     int currx = 0;
     int curry = 0;
 
-    map<pair<int,int>,int> to_num;
-    vector<pair<int,int>> to_coord(10001, {inf,inf});
+    map<int, int> to_num;
+    Coord to_coord[10001]; 
 
-    to_num[{currx,curry}] = 1;
-    to_coord[1] = {currx,curry};
+    to_num[get(currx,curry)] = 1;
+    to_coord[1] = Coord{currx,curry};
 
     int curr = 2;
     int steps = 1;
@@ -44,9 +87,9 @@ int main() {
                     currx++;
                 }
 
-                if(!prime(curr)) {
-                    to_num[{currx,curry}] = curr;
-                    to_coord[curr] = {currx,curry};
+                if(!isPrime(curr)) {
+                    to_num[get(currx,curry)] = curr;
+                    to_coord[curr] = Coord{currx,curry};
                 }
 
                 curr++;
@@ -60,9 +103,9 @@ int main() {
     
     for(int i = -9; i <= 9; i++) {
         for(int j = -9; j <= 9; j++) {
-           if (to_num.count({i,j}))
+           if (to_num.count(get(i,j)))
            {
-                cout << to_num[{i,j}] << "\t";
+                cout << to_num[get(i,j)] << "\t";
            }
            else 
            {
@@ -78,35 +121,36 @@ int main() {
     while(cin >> n1 >> n2) {
         cout << "Case " << num++ << ": ";
 
-        map<pair<int,int>,int> vis;
-        queue<pair<int,int>> q;
-        q.push(to_coord[n1]);
+        map<int, int> vis;
+        queue<int> q;
+        q.push(to_coord[n1].getInt());
 
         while(!q.empty()) {
-            pair<int,int> curr = q.front();
+            int curr = q.front();
             q.pop();
 
             for(int i = 0; i < 4; i++) {
-                int nextx = curr.first + dx[i];
-                int nexty = curr.second + dy[i];
+                Coord next = get(curr);
+                next.x += dx[i];
+                next.y += dy[i];
 
                 // Check if path is possible 
-                if(!to_num.count({nextx,nexty})) {
+                if(!to_num.count(get(next.x,next.y))) {
                     continue;
                 }
-                if(vis.count({nextx,nexty})) {
+                if(vis.count(next.getInt())) {
                     continue;
                 }
-                vis[{nextx,nexty}] = vis[curr] + 1;
-                q.push({nextx,nexty});
+                vis[next.getInt()] = vis[curr] + 1;
+                q.push(next.getInt());
             }
         }
 
-        if(vis[to_coord[n2]] == 0 && n1 != n2) {
+        if(vis[to_coord[n2].getInt()] == 0 && n1 != n2) {
             cout << "impossible" << endl;
         }
         else {
-            cout << vis[to_coord[n2]] << endl;
+            cout << vis[to_coord[n2].getInt()] << endl;
         }
     }
 }
