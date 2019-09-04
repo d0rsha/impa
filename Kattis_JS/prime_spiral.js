0aede87c3d036acbfdcc0427c3d12f95a378280e
Anders Vrethem <anders.vrethem@hiq.se>
2019-09-04 13:16:07 +0200

    Working

--- Kattis_JS/prime_spiral.js
+++ Kattis_JS/prime_spiral.js
@@ -86,12 +86,12 @@ function int(arr)
     }
 
 /*
-for(let i = -5; i <= +5; i++) 
+for(let i = -5; i <= 5; i++) 
     {
         let row = "";
         for(let j = -5; j <= 5; j++) {
            if (to_num[i+OFFSET][j+OFFSET] != undefined) {
-               row += to_coord[to_num[i+OFFSET][j+OFFSET]] + "\t";
+               row += to_num[i+OFFSET][j+OFFSET] + "\t";
            }
            else {
                row += " X " + "\t";
@@ -103,71 +103,66 @@ for(let i = -5; i <= +5; i++)
 
 let _case = 1;
 let src, dest;
-let reads = 0;
 rl.on('line', (line) => {
     if (!line) { rl.close(); return }
 
-    if (reads++ === 0) {
-        src = Number(line);
+    src = Number(line.split(" ")[0]);
+    dest = Number(line.split(" ")[1]);
+    let bfs_map = {};
+    let bfs_q = [];
+    let run = true;
+
+
+    // Skip if src || dest not reachable
+    if ((src == dest) || (isPrime(src) || isPrime(dest))) { 
+        run = false;
     } else {
-        dest = Number(line);
-        let bfs_map = {};
-        let bfs_q = [];
-        let run = true;
+        bfs_q.push([to_coord[src][0], to_coord[src][1]]);
+        bfs_map[(to_coord[src])] = 0;
+    } 
 
+    //
+    // Breadth First Search (BFS)
+    //
+    while(bfs_q.length > 0 && run) {
+        // console.log("Queue: ", bfs_q);
+        // console.log("Map: ", bfs_map)
+        const curr = bfs_q.shift();
 
-        // Skip if src || dest not reachable
-        if ((src == dest) || (isPrime(src) || isPrime(dest))) { 
-            run = false;
-        } else {
-            bfs_q.push([to_coord[src][0], to_coord[src][1]]);
-            bfs_map[(to_coord[src])] = 0;
-        } 
+        for(let i = 0; i < 4; i++) {
+            const nextx = curr[0] + Number(dx[i]);
+            const nexty = curr[1] + Number(dy[i]);
 
-        //
-        // Breadth First Search (BFS)
-        //
-        while(bfs_q.length > 0 && run) {
-            // console.log("Queue: ", bfs_q);
-            // console.log("Map: ", bfs_map)
-            const curr = bfs_q.shift();
+            // Check if path is possible 
+            if(nextx > 99 || nextx < 0 ||nexty > 99 || nexty < 0) {
+                continue;
+            }
+            
+            if(to_num[nextx][nexty] === undefined) {
+                continue;
+            }
+            // Check if there already exists shorter path to coordinate
+            else if(bfs_map[([nextx,nexty])] !== undefined) {
+                continue;
+            }
+            bfs_map[([nextx,nexty])] = Number(bfs_map[(curr)]) + 1; 
+            bfs_q.push([nextx,nexty]);
 
-            for(let i = 0; i < 4; i++) {
-                const nextx = curr[0] + Number(dx[i]);
-                const nexty = curr[1] + Number(dy[i]);
-
-                // Check if path is possible 
-                if(to_num[nextx][nexty] === undefined) {
-                    continue;
-                }
-                // Check if there already exists shorter path to coordinate
-                else if(bfs_map[([nextx,nexty])] !== undefined) {
-                    continue;
-                }
-                bfs_map[([nextx,nexty])] = Number(bfs_map[(curr)]) + 1; 
-                bfs_q.push([nextx,nexty]);
-
-                // With BFS the first occourance must also be shortest 
-                if (to_num[nextx][nexty] === dest) {
-                    run = false;
-                }
+            // With BFS the first occourance must also be shortest 
+            if (to_num[nextx][nexty] === dest) {
+                run = false;
             }
         }
-
-        
-        // If dest is not inside map then no path available
-        if ( src == dest ||  (isPrime(src) || isPrime(dest))) {
-            console.log("Case ", _case++, ": impossible");
-        } else if(bfs_map[to_coord[dest]] === undefined) {
-            console.log("Case ", _case++, ": impossible");
-        } else {
-            console.log("Case ", _case++, ": ", bfs_map[to_coord[dest]]);
-        }
-
-        
-
-        reads = 0;  
     }
 
     
+    // If dest is not inside map then no path available
+    if ( src == dest ||  (isPrime(src) || isPrime(dest))) {
+        console.log("Case", (_case++).toString(10) +  ": impossible");
+    } else if(bfs_map[to_coord[dest]] === undefined) {
+        console.log("Case", (_case++).toString(10) + ": impossible");
+    } else {
+        console.log("Case", (_case++).toString(10) + ":", bfs_map[to_coord[dest]]);
+    }
+
 });
