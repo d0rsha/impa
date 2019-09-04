94948dc69624fc45b2550e3d153c253d98d3006e
d0rshBooken <andvr916@student.liu.se>
2019-09-05 00:57:33 +0200

    Tested and Worki'n. Fan va snyggt dörshagit add knights_marathon.js Uhgit add knights_marathon.js Jahgit add knights_marathon.js

--- Kattis_JS/knights_marathon.js
+++ Kattis_JS/knights_marathon.js
@@ -1,11 +1,20 @@
 
+// The 8 possible moves of a knight 
+const jumpx = [-2, -1,  1,  2, -2, -1, 1, 2]; 
+const jumpy = [-1, -2, -2, -1,  1,  2, 2, 1]; 
+
+// Locie variables
+let x = 0;
+let y = 0;
+let cnt = 0;
+let size_x, size_y;
+let x1, y1, x2, y2;
+
+
 //
 // Algebraic solution O(1) time
 //
 
-/*
- * Five by five matrix for remaining jumps 
-*/
 function knight_jumps_left(dx_, dy_, first_move) 
 {
     let dx = Math.max(dx_, dy_);
@@ -34,15 +43,17 @@ function solve_algebraic_helper(dx_, dy_, dsx, dsy)
     let dx = Math.max(dx_, dy_);
     let dy = Math.min(dx_, dy_);
 
-
+    // within range 
     if (dx <= 2) { return knight_jumps_left(dx, dy, true); }
 
+    // calculate n_y and n_x after formula
     let ny = Math.max( Math.floor((2 * dy - dx -1) / 3), 0);
     let nx = Math.floor((dx - ny - 1) / 2);
 
     dx = dx - ( 2 * nx ) - ny;
     dy = (dy - ( 2 * ny ) - nx).mod(2);
     
+    // If destionation is just along a border
     if (dsx > nx * 2 && dsy > ny * 1) {
         return nx + ny + knight_jumps_left(dx, dy, false);
     } else {
@@ -52,7 +63,7 @@ function solve_algebraic_helper(dx_, dy_, dsx, dsy)
     
 function solve_algebraic(src_x, src_y, dest_x, dest_y, size_x, size_y)
 {
-    // Corner cases
+    // Declare all corner cases
     if (
         // Lower left corner
         src_x == 0 && src_y == 0 && dest_x == 1 && dest_y == 1 ||
@@ -71,13 +82,13 @@ function solve_algebraic(src_x, src_y, dest_x, dest_y, size_x, size_y)
             return 4;
         }
 
-    let deltax = Math.abs(src_x - dest_x);
-    let deltay = Math.abs(src_y - dest_y);
+    let dx = Math.abs(src_x - dest_x);
+    let dy = Math.abs(src_y - dest_y);
     
     let dsx = Math.abs(src_x - size_x);
     let dsy = Math.abs(src_y - size_y);
 
-    return solve_algebraic_helper(deltax, deltay, dsx, dsy);
+    return solve_algebraic_helper(dx, dy, dsx, dsy);
 }	
 
 
@@ -98,30 +109,22 @@ function bfs_minmum_jumps(src_x, src_y, dest_x, dest_y, size_x, size_y)
         return 0;
     }
 
-    // The 8 possible moves of a knight 
-    const dx = [-2, -1,  1,  2, -2, -1, 1, 2]; 
-    const dy = [-1, -2, -2, -1,  1,  2, 2, 1]; 
-
     let q = [];
     let map = {};
 
     q.push([src_x, src_y]);
     map[[src_x, src_y]] = 0;
 
-    let x = 0;
-    let y = 0;
-    let searching = true;
-
-    // loop untill we have one element in queue 
+    // Loop until queue is empty or function returns 
     while ( q.length > 0 )
     { 
         let coord = q.shift();
 
         // loop for all reachable states 
-        for (let i = 0; i < dx.length; i++) 
+        for (let i = 0; i < jumpx.length; i++) 
         { 
-            x = coord[0] + dx[i]; 
-            y = coord[1] + dy[i]; 
+            x = coord[0] + jumpx[i]; 
+            y = coord[1] + jumpy[i]; 
   
             // If reachable state is not yet visited and 
             // inside board, push that state into queue 
@@ -131,6 +134,7 @@ function bfs_minmum_jumps(src_x, src_y, dest_x, dest_y, size_x, size_y)
                 q.push([x, y]);
                 map[[x, y]] = map[coord] + 1;
 
+                // we have reached our destination, yeey!
                 if ( x === dest_x && y === dest_y) {
                     return map[[x, y]];
                 }
@@ -148,13 +152,10 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-let cnt = 0;
-let size_x, size_y;
-let x1, y1, x2, y2;
 rl.on('line', (line) => {
     if (!line) { rl.close(); return; }
 
-    if (cnt++ === 0) { /* Throw away first line */ 
+    if (cnt++ === 0) { 
         size_x = line.split(" ")[0]; size_y = line.split(" ")[1];
     }
     else if (cnt == 2) { 
@@ -179,18 +180,13 @@ rl.on('line', (line) => {
             //           solve for each case, pick optimal case
 
             const end = [x2, y2];
-            let x;
-            let y;
             let coord;
-            // The 8 possible moves of a knight 
-            const dx = [-2, -1,  1,  2, -2, -1, 1, 2]; 
-            const dy = [-1, -2, -2, -1,  1,  2, 2, 1]; 
-            
             let q = [];
-                // loop for all reachable states 
-            for (let i = 0; i < dx.length; i++) { 
-                x = end[0] + dx[i]; 
-                y = end[1] + dy[i]; 
+
+            // loop for all reachable states 
+            for (let i = 0; i < jumpx.length; i++) { 
+                x = end[0] + jumpx[i]; 
+                y = end[1] + jumpy[i]; 
 
                 if (withinBounds(x,y,size_x,size_y)) {
                     q.push(solve_algebraic(x1, y1, x, y, size_x, size_y) + 1)
@@ -199,6 +195,7 @@ rl.on('line', (line) => {
 
             //console.log(q);
 
+            // Pick best case 
             let best = q.shift();
             for (let i=0; i < q.length; ) {
                 let cand = q.shift();
@@ -208,7 +205,6 @@ rl.on('line', (line) => {
                 }
             }
             console.log(best);
-
         }
 
         rl.close();
